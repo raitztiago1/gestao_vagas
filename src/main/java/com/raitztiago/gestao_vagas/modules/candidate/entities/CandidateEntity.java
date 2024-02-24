ab80e412139e4b504d22020843485b75c3d4bd84
Tiago Raitz <60758361+raitztiago1@users.noreply.github.com>
2024-02-24 19:20:04 -0300

    Create_DB

--- src/main/java/com/raitztiago/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/com/raitztiago/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -1,28 +1,47 @@
 package com.raitztiago.gestao_vagas.modules.candidate.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@NoArgsConstructor
+@Entity(name = "candidate")
 @AllArgsConstructor
+@NoArgsConstructor
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
-    @NotBlank(message = "O campo [Username] não pode estar vazio")
+
+    @NotBlank()
     @Pattern(regexp = "\\S+", message = "O campo [Username] não deve conter espaço.")
     private String username;
+
     @Email(message = "O campo [E-mail] deve conter um E-mail válido!")
     private String email;
+
     @Length(min = 10, max = 100, message = "O campo [Senha] deve conter entre 10 e 100 caracteres!")
     private String password;
+
     private String description;
+
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
