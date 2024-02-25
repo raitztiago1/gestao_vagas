36b83957375494f8d09b1c637d6cd58bc42fcd92
Tiago Raitz <60758361+raitztiago1@users.noreply.github.com>
2024-02-24 21:16:12 -0300

    Creation of Company and Jobs entitys and mapping

--- src/main/java/com/raitztiago/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/com/raitztiago/gestao_vagas/exceptions/UserFoundException.java
@@ -2,6 +2,6 @@ package com.raitztiago.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException {
     public UserFoundException() {
-        super("Usuário Já Existe");
+        super("User Already Exists");
     }
 }
---
+++ src/main/java/com/raitztiago/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,12 @@
+package com.raitztiago.gestao_vagas.modules.company.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raitztiago.gestao_vagas.modules.company.entities.CompanyEntity;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+    Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+}
