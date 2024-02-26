710147ec374be9170d6d3b762e9aa40bf793b09b
Tiago Raitz <60758361+raitztiago1@users.noreply.github.com>
2024-02-26 14:16:06 -0300

    Implementation: Password authentication and Crypto.

--- src/main/java/com/raitztiago/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/raitztiago/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,4 +1,20 @@
 package com.raitztiago.gestao_vagas.modules.company.useCases;
 
-public class AuthCompany {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.raitztiago.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.raitztiago.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class AuthCompanyUseCase {
+    
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public void execute(AuthCompanyDTO authCompanyDTO) {
+
+        
+
+    }
 }
