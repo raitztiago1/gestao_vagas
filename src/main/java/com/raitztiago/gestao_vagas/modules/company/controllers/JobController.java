09bd8ecbb69cc82daac298b2417b8672b89a326c
Tiago Raitz <60758361+raitztiago1@users.noreply.github.com>
2024-03-01 10:28:00 -0300

    Create DTO of Job, refactoring JobController.java

--- src/main/java/com/raitztiago/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/raitztiago/gestao_vagas/modules/company/controllers/JobController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.raitztiago.gestao_vagas.modules.company.dto.CreateJobDTO;
 import com.raitztiago.gestao_vagas.modules.company.entities.JobEntity;
 import com.raitztiago.gestao_vagas.modules.company.useCases.CreateJobUseCase;
 
@@ -22,16 +23,19 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public JobEntity create(@Valid @RequestBody JobEntity jobEntity, HttpServletRequest request) {
+    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
 
-        try {
-            var companyId = request.getAttribute("company_id");
-            jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
-            return this.createJobUseCase.execute(jobEntity);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return jobEntity;
-        }
+        var companyId = request.getAttribute("company_id");
+
+        var jobEntity = JobEntity.builder()
+                .benefits(createJobDTO.getBenefits())
+                .companyId(UUID.fromString(companyId.toString()))
+                .description(createJobDTO.getDescription())
+                .level(createJobDTO.getLevel())
+                .build();
+
+        return this.createJobUseCase.execute(jobEntity);
+
     }
 
 }
